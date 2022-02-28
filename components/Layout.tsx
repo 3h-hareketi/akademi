cad1b6cb043aa506556426a3d2e3f6bd5161e707
Ant Somers <antsomers@gmail.com>
2022-02-28 03:28:06 +0300

    Fix missing layout props type

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,12 @@
+import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-const Layout = ({ children }) => {
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: Props) => {
   return (
     <div className="content">
       <Navbar />
