5fe2cbddb0a66be7d889b8a7e611e626f0243e4a
Yanki <yankisirtas@hotmail.com>
2022-03-03 15:28:49 +0200

    Revert hardcoded url

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,3 @@
 import { GraphQLClient } from "graphql-request";
 
-export const client = new GraphQLClient(
-  "https://api-eu-central-1.graphcms.com/v2/cl05d9jaj1rly01z24s4l5cwj/master"
-);
+export const client = new GraphQLClient(process.env.GRAPHQL_ENDPOINT!);
