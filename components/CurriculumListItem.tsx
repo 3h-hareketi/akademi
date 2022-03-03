bd53a966983370400c464fc0baef096fccd67043
Yanki <yankisirtas@hotmail.com>
2022-03-03 18:41:05 +0200

    Fix missing property error

--- components/CurriculumListItem.tsx
+++ components/CurriculumListItem.tsx
@@ -21,11 +21,6 @@ const CurriculumListItem = ({ curriculum }: Props) => (
           />
         </div>
       </div>
-      {/* <img
-      className="object-cover w-full h-full rounded"
-      src="https://images.unsplash.com/photo-1552338804-c42590cb7b88?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1189&amp;q=80"
-      alt=""
-    /> */}
     </div>
     <div className="w-full px-3 lg:w-3/4">
       <Link href={`/${curriculum.category}/${curriculum.slug}`} passHref>
@@ -40,7 +35,7 @@ const CurriculumListItem = ({ curriculum }: Props) => (
           className="text-primary hover:underline hover:text-primaryShade"
           href="#"
         >
-          {curriculum.category}
+          {curriculum.category?.title}
         </a>
         <span className="mx-2 text-gray-400">•</span>
         <span className="text-gray-400">8 konu</span>
