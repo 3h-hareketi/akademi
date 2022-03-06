7ff94306a9d9aa87f8fc44d3f8268db6b47b030d
Yanki <yankisirtas@hotmail.com>
2022-03-06 22:09:05 +0200

    Install headless-ui

---
+++ pages/exam.tsx
@@ -0,0 +1,15 @@
+const Exam = () => {
+  return <div className="py-10 mx-auto bg-gray-50"></div>;
+};
+
+// export async function getStaticProps() {
+//   const sdk = getSdk(client);
+//   const { curricula } = await sdk.Exam();
+//   return {
+//     props: {
+//       exam,
+//     },
+//   };
+// }
+
+export default Exam;
