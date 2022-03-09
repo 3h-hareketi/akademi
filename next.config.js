0028e0803549e1e7960b6522691dc3b91a943617
Ant Somers <antsomers@gmail.com>
2022-03-10 02:47:13 +0300

    Fix sentry settings

--- next.config.js
+++ next.config.js
@@ -5,8 +5,6 @@
 
 const { withSentryConfig } = require("@sentry/nextjs");
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
-
 const SENTRY_AUTH_TOKEN = process.env.SENTRY_AUTH_TOKEN;
 
 /** @type {import('next').NextConfig} */
@@ -27,7 +25,6 @@ const sentryWebpackPluginOptions = {
   silent: true, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
-  dsn: SENTRY_DSN,
   authToken: SENTRY_AUTH_TOKEN,
 };
 
