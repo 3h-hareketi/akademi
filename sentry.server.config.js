1a2be5d5edc941950a1c2359d17b02bf63729c51
Ant Somers <antsomers@gmail.com>
2022-03-10 02:49:41 +0300

    Adjust traceSampleRate

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -6,12 +6,14 @@ import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
+const env = process.env.NODE_ENV;
+
 Sentry.init({
   dsn:
     SENTRY_DSN ||
     "https://81f87bae6c0c4c2397bcc76461d16510@o1153095.ingest.sentry.io/6231850",
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
+  tracesSampleRate: env === "production" ? 1.0 : 0.2,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
