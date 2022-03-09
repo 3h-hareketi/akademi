26c280cdeb537538ef4fefa3021cdb5be5d169c5
Ant Somers <antsomers@gmail.com>
2022-03-10 02:30:01 +0300

    Revert "Add tracesSampleRate to sentry config"
    This reverts commit cd82fc4f607de3239432e3da33fccde9c6e2a038.

--- next.config.js
+++ next.config.js
@@ -9,8 +9,6 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 const SENTRY_AUTH_TOKEN = process.env.SENTRY_AUTH_TOKEN;
 
-const env = process.env.NODE_ENV;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -31,7 +29,6 @@ const sentryWebpackPluginOptions = {
   // https://github.com/getsentry/sentry-webpack-plugin#options.
   dsn: SENTRY_DSN,
   authToken: SENTRY_AUTH_TOKEN,
-  tracesSampleRate: env === "production" ? 0.2 : 1,
 };
 
 module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
