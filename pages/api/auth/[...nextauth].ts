824c70acd93696053d2efc743f7f31b73a877541
Ant Somers <antsomers@gmail.com>
2022-03-12 04:04:57 +0300

    Install and integrate next-auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { FaunaAdapter } from "@next-auth/fauna-adapter";
+import { client } from "../../../lib/faunaClient";
+
+export default NextAuth({
+  adapter: FaunaAdapter(client),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+});
