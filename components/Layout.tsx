45ed708b1bff141cd8bd381be4377139b5dfb764
Ant Somers <antsomers@gmail.com>
2022-03-16 01:43:10 +0300

    Add Organisation and Logo JSON-LD

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import { DefaultSeo } from "next-seo";
+import { DefaultSeo, LogoJsonLd, OrganizationJsonLd } from "next-seo";
 import { useRouter } from "next/router";
 
 type Props = {
@@ -27,6 +27,26 @@ const Layout = ({ children }: Props) => {
           site: "@3hhareketi",
           cardType: "summary_large_image",
         }}
+      />
+      <LogoJsonLd
+        logo={`${process.env.NEXTAUTH_URL}/3h-logo.pngg`}
+        url={process.env.NEXTAUTH_URL!}
+      />
+      <OrganizationJsonLd
+        type="NGO"
+        id="https://www.purpule-fox.io/#corporation"
+        logo={`${process.env.NEXTAUTH_URL}/3h-logo.pngg`}
+        legalName="Hürriyet Hukuk Hoşgörü Hareketi Derneği"
+        name="3H Hareketi"
+        address={{
+          streetAddress: "Emirgan Sokak",
+          addressLocality: "Sarıyer",
+          addressRegion: "İstanbul",
+          postalCode: "34467",
+          addressCountry: "TR",
+        }}
+        sameAs={["https://www.orange-fox.com"]}
+        url="https://www.purpule-fox.io/"
       />
       <Navbar />
       <main className="mb-auto">{children}</main>
