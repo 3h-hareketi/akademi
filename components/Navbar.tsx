268b876903bde522bafe33b10517faf69151e2fe
Yanki <yankisirtas@hotmail.com>
2022-03-15 23:18:40 +0200

    Remove unused import

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
