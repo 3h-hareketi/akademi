2dc84429235f042d4652092577bbef25ea7e7a6d
Berke Doruk Ozden <berked.ozden@gmail.com>
2022-03-17 08:36:30 +0300

    escape char fix

---
+++ pages/404.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function custom404() {
+  return (
+    <div className="py-10 md:px-10 radius-for-skewed bg-gray-50">
+      <div className="mx-auto">
+        <div className="grid justify-items-center">
+          <Image
+            className="object-cover"
+            src="/404.png"
+            alt="3H Movement-3H Hareketi"
+            width={500}
+            height={355}
+          />
+          <h1 className="text-2xl font-bold ">
+            Aradığınız sayfa taşınmış, kaldırılmış veya hiç var olmamış
+            olabilir!
+          </h1>
+          <p className="text-xl font-semibold">
+            Bunun yerine{" "}
+            <Link href="/">
+              <a className="font-bold text-primary-500 decoration-primary-500">
+                Anasayfa&#39;ya
+              </a>
+            </Link>{" "}
+            gidin.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
