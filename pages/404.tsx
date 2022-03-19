d414decd54ba38752778594378b2d8b384b4dea4
Berke Doruk Ozden <berked.ozden@gmail.com>
2022-03-20 02:32:13 +0300

    Fix grid-to-flex & Add placeholder

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,15 +3,16 @@ import Link from "next/link";
 
 export default function custom404() {
   return (
-    <div className="py-10 md:px-10 radius-for-skewed bg-gray-50">
-      <div className="mx-auto">
-        <div className="grid justify-items-center">
+    <div className="py-10 text-center md:px-10 radius-for-skewed bg-gray-50">
+      <div className="flex flex-wrap justify-center mx-auto">
+        <div className="py-20 basis-1/2">
           <Image
             className="object-cover"
             src="/404page.png"
             alt="3H Movement-3H Hareketi"
             width={270}
             height={300}
+            placeholder="blurDataURL"
           />
           <p className="py-3 text-l">
             Aradığınız sayfa taşınmış, kaldırılmış veya hiç var olmamış
