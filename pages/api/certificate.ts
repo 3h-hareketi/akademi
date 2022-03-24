97690d964505d9e95179e18785800c444dde9bfa
Yanki <yankisirtas@hotmail.com>
2022-03-24 14:50:53 +0200

    Merge pdf generation functions

--- pages/api/certificate.ts
+++ pages/api/certificate.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
-import generatePdf from "../../utils/generatePdf";
+import { Session } from "next-auth";
+import path from "path";
+import { PDFDocument } from "pdf-lib";
 
 export default async function handler(
   request: NextApiRequest,
@@ -18,4 +20,21 @@ export default async function handler(
     },
     body: buffer.toString("base64"),
   });
+}
+async function generatePdf(session: Session | null) {
+  const formUrl = path.join(
+    "http://localhost:3000",
+    "certificate_template.pdf"
+  ); // TODO
+  const formPdfBytes = await fetch(formUrl).then((res) => res.arrayBuffer());
+  const pdfDoc = await PDFDocument.load(formPdfBytes);
+
+  const form = pdfDoc.getForm();
+
+  const nameField = form.getTextField("Ad Soyad");
+
+  nameField.setText(session?.user?.name || "Kullanici Adi Yok");
+
+  const pdfBytes = await pdfDoc.save();
+  return pdfBytes;
 }
