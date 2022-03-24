f47ab62f04ff5d9c90808d33800d7c273195e0ba
Yanki <yankisirtas@hotmail.com>
2022-03-24 18:46:22 +0200

    Add custom font

--- pages/api/certificate.ts
+++ pages/api/certificate.ts
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
-import { PDFDocument } from "pdf-lib";
-
+import { PDFDocument, StandardFonts, TextAlignment } from "pdf-lib";
+import fontkit from "@pdf-lib/fontkit";
+import fs from "fs";
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
@@ -13,18 +14,32 @@ export default async function handler(
       ? "https://" + process.env.VERCEL_URL
       : "http://localhost:3000" + "/certificate_template.pdf";
 
+  const fontUrl =
+    process.env.NODE_ENV === "production"
+      ? "https://" + process.env.VERCEL_URL
+      : "http://localhost:3000" + "/Merriweather-Italic.ttf";
+
   const formPdfBytes = await fetch(formUrl).then((res) => res.arrayBuffer());
   const pdfDoc = await PDFDocument.load(formPdfBytes);
+
+  pdfDoc.registerFontkit(fontkit);
+  const fontBytes = await fetch(fontUrl).then((res) => res.arrayBuffer());
+  const customFont = await pdfDoc.embedFont(fontBytes);
 
   const form = pdfDoc.getForm();
 
   const nameField = form.getTextField("katilimcinin adi soyadi");
   nameField.setText(session?.user?.name || "Kullanici Adi Yok");
   nameField.enableReadOnly();
+  nameField.setAlignment(TextAlignment.Center);
 
+  nameField.updateAppearances(customFont);
   const curriculumField = form.getTextField("egitimin adi");
   curriculumField.setText("Sertifika #1");
   curriculumField.enableReadOnly();
+  curriculumField.setAlignment(TextAlignment.Center);
+
+  curriculumField.updateAppearances(customFont);
 
   const pdfBytes = await pdfDoc.save();
 
