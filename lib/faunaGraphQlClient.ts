6ffa79a5f5f01a53c94788e2f4741f016d5ca42a
Ant Somers <antsomers@gmail.com>
2022-03-25 05:42:38 +0300

    Add missing authorization header to faunaGraphQlCLient

--- lib/faunaGraphQlClient.ts
+++ lib/faunaGraphQlClient.ts
@@ -1,3 +1,5 @@
 import { GraphQLClient } from "graphql-request";
 
-export const client = new GraphQLClient(process.env.FAUNA_ENDPOINT!);
+export const client = new GraphQLClient(process.env.FAUNA_ENDPOINT!, {
+  headers: { Authorization: `Bearer ${process.env.FAUNA_SECRET}` },
+});
