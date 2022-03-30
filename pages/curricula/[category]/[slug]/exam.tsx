246e3b2f1f937bdb6a8c7c0d74e31ae1f7cc4c06
Ant Somers <antsomers@gmail.com>
2022-03-30 15:09:51 +0300

    Add noindex to exam page

--- pages/curricula/[category]/[slug]/exam.tsx
+++ pages/curricula/[category]/[slug]/exam.tsx
@@ -9,6 +9,7 @@ import { client } from "../../../../lib/graphCmsClient";
 import { Fragment, useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
+import { NextSeo } from "next-seo";
 
 type Props = {
   curriculum: Curriculum;
@@ -34,69 +35,72 @@ const Exam = ({ curriculum }: Props) => {
   };
 
   return (
-    <div className="py-10 mx-auto bg-gray-50">
-      <div className="max-w-screen-xl px-4 py-10 mx-auto sm:px-6 lg:py-12 lg:px-8">
-        <h2 className="mb-10 text-4xl font-bold lg:text-5xl font-heading">
-          {curriculum.title}
-        </h2>
-        {curriculum.articles.map((article) => (
-          <Disclosure key={article.id} as="div" className="mt-6">
-            {({ open }) => (
-              <>
-                <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left rounded-lg text-primary-900 bg-primary-100 hover:bg-primary-200 focus:outline-none focus-visible:ring focus-visible:ring-primaryShade focus-visible:ring-opacity-75 h-14">
-                  <span className="my-auto">{article.title}</span>
-                  <ChevronUpIcon
-                    className={`${
-                      !open ? "transform rotate-180" : ""
-                    } w-5 h-5 text-primary-500`}
-                  />
-                </Disclosure.Button>
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
-                  <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                    <div className="p-5">
-                      {" "}
-                      <Interweave
-                        content={article.content.html}
-                        allowList={new Array(...ALLOWED_TAG_LIST, "iframe")}
-                      />
-                      {article.choices && (
-                        <Choices
-                          choices={article.choices}
-                          correctAnswerCount={correctAnswerCount}
-                          setCorrectAnswerCount={setCorrectAnswerCount}
+    <>
+      <NextSeo noindex={true} />
+      <div className="py-10 mx-auto bg-gray-50">
+        <div className="max-w-screen-xl px-4 py-10 mx-auto sm:px-6 lg:py-12 lg:px-8">
+          <h2 className="mb-10 text-4xl font-bold lg:text-5xl font-heading">
+            {curriculum.title}
+          </h2>
+          {curriculum.articles.map((article) => (
+            <Disclosure key={article.id} as="div" className="mt-6">
+              {({ open }) => (
+                <>
+                  <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left rounded-lg text-primary-900 bg-primary-100 hover:bg-primary-200 focus:outline-none focus-visible:ring focus-visible:ring-primaryShade focus-visible:ring-opacity-75 h-14">
+                    <span className="my-auto">{article.title}</span>
+                    <ChevronUpIcon
+                      className={`${
+                        !open ? "transform rotate-180" : ""
+                      } w-5 h-5 text-primary-500`}
+                    />
+                  </Disclosure.Button>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                      <div className="p-5">
+                        {" "}
+                        <Interweave
+                          content={article.content.html}
+                          allowList={new Array(...ALLOWED_TAG_LIST, "iframe")}
                         />
-                      )}
-                    </div>
-                  </Disclosure.Panel>
-                </Transition>
-              </>
-            )}
-          </Disclosure>
-        ))}
-        <div className="flex mt-12">
-          {" "}
-          <button
-            className={`${
-              isSubmitting
-                ? "bg-gray-600"
-                : "bg-primary-500 hover:bg-primary-700"
-            } px-6 py-2 ml-auto font-bold leading-loose transition duration-200 md:inline-block rounded-l-xl rounded-t-xl text-gray-50`}
-            onClick={submitResult}
-            disabled={isSubmitting}
-          >
-            Tamamla
-          </button>
+                        {article.choices && (
+                          <Choices
+                            choices={article.choices}
+                            correctAnswerCount={correctAnswerCount}
+                            setCorrectAnswerCount={setCorrectAnswerCount}
+                          />
+                        )}
+                      </div>
+                    </Disclosure.Panel>
+                  </Transition>
+                </>
+              )}
+            </Disclosure>
+          ))}
+          <div className="flex mt-12">
+            {" "}
+            <button
+              className={`${
+                isSubmitting
+                  ? "bg-gray-600"
+                  : "bg-primary-500 hover:bg-primary-700"
+              } px-6 py-2 ml-auto font-bold leading-loose transition duration-200 md:inline-block rounded-l-xl rounded-t-xl text-gray-50`}
+              onClick={submitResult}
+              disabled={isSubmitting}
+            >
+              Tamamla
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
