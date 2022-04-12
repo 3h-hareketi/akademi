a50684528ee478592a4460c79b10315a9d9b770b
Ant Somers <antsomers@gmail.com>
2022-04-12 23:12:15 +0300

    Fix format

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "../components/Layout";
 import { SessionProvider } from "next-auth/react";
+import Layout from "../components/Layout";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
+    <SessionProvider session={session}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
