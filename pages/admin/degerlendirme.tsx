f3709b40a2a9596b8a2f4279d75f3d5495057b21
Ant Somers <antsomers@gmail.com>
2022-04-12 19:29:25 +0300

    Add article prop to Validation

--- pages/admin/degerlendirme.tsx
+++ pages/admin/degerlendirme.tsx
@@ -2,10 +2,17 @@ import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react";
 import { NextSeo } from "next-seo";
 import { getSdk, Submission } from "../../interfaces/fauna";
+import {
+  Article,
+  ArticleByIdQuery,
+  getSdk as getGraphCmsSdk,
+} from "../../interfaces/graphcms";
 import { client } from "../../lib/faunaGraphQlClient";
+import { client as graphCmsClient } from "../../lib/graphCmsClient";
 
 type Props = {
   submission: Submission;
+  article: Article[];
 };
 
 const Validation = ({ submission }: Props) => {
@@ -46,8 +53,15 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     id: context.params.id as string,
   });
 
+  const graphCmsSdk = getGraphCmsSdk(graphCmsClient);
+  const articles: ArticleByIdQuery[] = [];
+
+  for (const article of submission?.answers?.data!) {
+    articles.push(await graphCmsSdk.ArticleById({ id: article?.articleId! }));
+  }
+
   return {
-    props: { submission: submission },
+    props: { submission: submission, articles: articles },
   };
 };
 
