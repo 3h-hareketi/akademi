896d5527f69e4f113947a2ce7180a86f0aa0731a
Ant Somers <antsomers@gmail.com>
2022-04-17 02:07:14 +0300

    Fix EmailProvider sender address

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -16,7 +16,7 @@ export default NextAuth({
   providers: [
     EmailProvider({
       server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM || "noreply@3hhareketi.org",
+      from: process.env.EMAIL_FROM || "3H Akademi <noreply@3hhareketi.org>",
       async sendVerificationRequest({
         identifier: email,
         url,
