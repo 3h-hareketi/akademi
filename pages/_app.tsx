30cf9a436621f7d0f593acfdfc2eae3540249427
Ant Somers <antsomers@gmail.com>
2022-10-18 15:53:09 +0300

    chore: add gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,20 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+      `}
+      </Script>
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -81,6 +81,11 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;" />`,
+          }}
+        />
       </body>
     </Html>
   );
